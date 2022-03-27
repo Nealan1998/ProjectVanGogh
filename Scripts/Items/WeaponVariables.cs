682c2d22fdfa4a74afbdf4ffbf97e575bbffe747
Nealan1998 <60358595+Nealan1998@users.noreply.github.com>
2022-03-27 15:43:48 -0600

    Machine Gun

---
+++ Scripts/Items/WeaponVariables.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponVariables 
+{
+    public WeaponTypes weaponType;
+    public string weaponName;
+    public float speed;
+    public int maximumAmount;
+    public Sprite sprite;
+    
+}
